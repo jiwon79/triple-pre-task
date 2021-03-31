a259616f4767d8d4600cf73760e7cf6da5671c7b
SeongMin Han <hsm950825@naver.com>
2021-03-31 18:27:57 +0900

    Update : 컨텐츠로고 디자인 변경

--- src/components/ContentLogo.tsx
+++ src/components/ContentLogo.tsx
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import { TRIPLE_LOGO_IMG } from "constants/images";
+
+const ContentLogoContainer = styled.div`
+  width: 400px;
+  height: 338px;
+  padding-top: 280px;
+  background-image: url(${TRIPLE_LOGO_IMG});
+  background-repeat: no-repeat;
+  background-size: 400px 338px;
+  text-align: center;
+  font-family: sans-serif;
+  font-size: 15px;
+  color: rgba(58, 58, 58, 0.7);
+`;
 
 const ContentLogo: React.FC = () => {
-  return <div>2019년 2월 기준</div>;
+  return <ContentLogoContainer>2019년 2월 기준</ContentLogoContainer>;
 };
 
 export default ContentLogo;
