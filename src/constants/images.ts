7972ae795d5981f7611205fa76848aa3ba920e83
SeongMin Han <hsm950825@naver.com>
2021-03-31 18:15:12 +0900

    Add : Image Constant Settings

---
+++ src/constants/images.ts
@@ -0,0 +1,7 @@
+import AppStore from "images/app-store@2x.png";
+import PlayStore from "images/play-store@2x.png";
+import TripleLogo from "images/triple@2x.png";
+
+export const APP_STORE_IMG = AppStore;
+export const PLAY_STORE_IMG = PlayStore;
+export const TRIPLE_LOGO_IMG = TripleLogo;
