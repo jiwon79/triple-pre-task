37f030629bdbca3157dc5a5c73ad69492ac5916d
SeongMin Han <hsm950825@naver.com>
2021-03-31 18:29:51 +0900

    Add : 측정지표 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import GlobalStyles from "styles/globalStyles";
 import ContentLogo from "components/ContentLogo";
+import Metrics from "components/Metrics";
 
 const App: React.FC = () => {
   return (
     <div>
       <GlobalStyles />
       <ContentLogo />
+      <Metrics />
     </div>
   );
 };
---
+++ src/components/Metrics.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Metrics: React.FC = () => {
+  return <div>Metrics Components</div>;
+};
+
+export default Metrics;
