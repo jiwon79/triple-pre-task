65b267af5c9fc38962ef0cafbad28568f698929e
SeongMin Han <hsm950825@naver.com>
2021-03-31 18:35:19 +0900

    Update : 측정지표 컴포넌트 디자인 변경

--- src/components/Metrics.tsx
+++ src/components/Metrics.tsx
@@ -1,7 +1,36 @@
 import React from "react";
+import styled from "styled-components";
+
+const MetricsContainer = styled.div`
+  margin-bottom: 50px;
+`;
+
+const MetricItem = styled.div`
+  margin-bottom: 20px;
+  font-family: sans-serif;
+  font-size: 36px;
+  color: rgb(58, 58, 58);
+  letter-spacing: -1px;
+
+  &:last-child {
+    margin-bottom: 0px;
+  }
+`;
 
 const Metrics: React.FC = () => {
-  return <div>Metrics Components</div>;
+  return (
+    <MetricsContainer>
+      <MetricItem>
+        <strong>350만 명</strong>의 사용자
+      </MetricItem>
+      <MetricItem>
+        <strong>21만 개</strong>의 리뷰
+      </MetricItem>
+      <MetricItem>
+        <strong>650만 개</strong>의 저장
+      </MetricItem>
+    </MetricsContainer>
+  );
 };
 
 export default Metrics;
