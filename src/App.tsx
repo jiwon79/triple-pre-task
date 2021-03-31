f40baee5e7340e9bcfad0258571c21c7cb57fbc2
SeongMin Han <hsm950825@naver.com>
2021-03-31 18:37:05 +0900

    Add : 어워드 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import GlobalStyles from "styles/globalStyles";
 import ContentLogo from "components/ContentLogo";
 import Metrics from "components/Metrics";
+import Award from "components/Award";
 
 const App: React.FC = () => {
   return (
@@ -9,6 +10,7 @@ const App: React.FC = () => {
       <GlobalStyles />
       <ContentLogo />
       <Metrics />
+      <Award />
     </div>
   );
 };
---
+++ src/components/Award.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Awards: React.FC = () => {
+  return <div>Awards Components</div>;
+};
+
+export default Awards;
