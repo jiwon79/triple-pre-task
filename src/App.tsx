32e2e34ab211fbb6f450baf7807ac70947c71f18
SeongMin Han <hsm950825@naver.com>
2021-03-31 18:24:30 +0900

    Add : 컨텐츠로고 컴포넌트 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import GlobalStyles from "styles/globalStyles";
+import ContentLogo from "components/ContentLogo";
 
 const App: React.FC = () => {
   return (
     <div>
       <GlobalStyles />
+      <ContentLogo />
     </div>
   );
 };
---
+++ src/components/ContentLogo.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ContentLogo: React.FC = () => {
+  return <div>2019년 2월 기준</div>;
+};
+
+export default ContentLogo;
