7ebec0930d308fb6e6434b581b48df2a0f41b71f
SeongMin Han <hsm950825@naver.com>
2021-03-31 18:46:56 +0900

    Update : 화면 레이아웃 재배치

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,41 @@
 import React from "react";
-import GlobalStyles from "styles/globalStyles";
+import styled from "styled-components";
 import ContentLogo from "components/ContentLogo";
 import Metrics from "components/Metrics";
 import Award from "components/Award";
+import GlobalStyles from "styles/globalStyles";
+
+const ResponsiveContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  width: 1200px;
+  height: 100%;
+  margin: 0 auto;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 100%;
+`;
 
 const App: React.FC = () => {
   return (
-    <div>
+    <>
       <GlobalStyles />
-      <ContentLogo />
-      <Metrics />
-      <Award />
-    </div>
+      <main>
+        <ResponsiveContainer>
+          <ContentContainer>
+            <ContentLogo />
+            <div>
+              <Metrics />
+              <Award />
+            </div>
+          </ContentContainer>
+        </ResponsiveContainer>
+      </main>
+    </>
   );
 };
 
